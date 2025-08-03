f462e79f4b63a48b72a1bc2bace2e26c508ac735
OtavioMR <otavvioo11@gmail.com>
2025-08-02 22:41:37 -0300

    Att portfolio

--- portfolio-spa/src/App.js
+++ portfolio-spa/src/App.js
@@ -1,145 +1,215 @@
 import React from 'react';
 import minhaFoto from './assets/images/MinhaFotoPerfil.jpeg';
-import fotoProjeto from './assets/images/projeto2.png'
+import fotoProjeto from './assets/images/projeto2.png';
 import certificadoPython from './assets/images/certificadoDigital.pdf';
+import fotoDidi from './assets/images/fotoDidi.jpg';
+import imagemFaculdade from './assets/images/imagemFaculdade.png';
+import fotoCertificado from './assets/images/fotoCertificado.png';
 import './App.css';
 
 function App() {
-
-  const projetos = [
-    { titulo: "ChronosLux", descricao: "Ecommerce de relógios", imagem: fotoProjeto },
-    { titulo: "ChronosLux", descricao: "Ecommerce de relógios", imagem: fotoProjeto },
-    { titulo: "ChronosLux", descricao: "Ecommerce de relógios", imagem: fotoProjeto },
-    { titulo: "ChronosLux", descricao: "Ecommerce de relógios", imagem: fotoProjeto }
-  ];
-
-  const cargos = [
-    { nomeCargo: "Desenvolvedor de projetos VR na USP", dataCargo: "(06/11/2006) - atualmente", responsabilidadesCargos: ["Criação de ambientes em realidade virtual", "Modelagem 3d"] },
-    { nomeCargo: "Freelancer", dataCargo: "(26-12-2025)", responsabilidadesCargos: ["Criar portfólios", "Criar ecommerce"] }
-  ];
-
-
   return (
     <>
-      <header className='App-header'>
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark colorNavBar">
-          <div className="container my-2">
-            <a className="navbar-brand" href="#sobre"><h1>Otávio Monteiro</h1></a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <header>
+        <nav className="navbar navbar-expand-sm bg-body-tertiary p-3">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#"><b>Otávio Monteiro</b></a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item"><a className="nav-link" href="#sobre">Sobre</a></li>
-                <li className="nav-item"><a className="nav-link" href="#projetos">Projetos</a></li>
-                <li className="nav-item"><a className="nav-link" href="#contato">Contato</a></li>
-              </ul>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-link" aria-current="page" href="#">Home</a>
+                <a className="nav-link" href="#">Features</a>
+                <a className="nav-link" href="#">Pricing</a>
+              </div>
             </div>
           </div>
         </nav>
       </header>
 
-      <section className="d-flex justify-content-center align-items-center p-5 Foto-Perfil">
-        <img src={minhaFoto} alt="Minha Foto" className="img-fluid minhaFoto" />
-      </section>
+      <main className='container py-4'>
 
-      <div className="container text-center" style={{ paddingTop: '80px' }}>
-        {/* SOBRE MIM */}
-        <section id="sobre" className="my-5 mx-3">
-          <h2 className="Sublinhado mb-4">Sobre Mim</h2>
-          <p>
-            Olá! Meu nome é <strong>Otávio Monteiro</strong>, sou estudante de <strong>Desenvolvimento de Software Multiplataforma</strong> na <strong>Fatec de Cotia</strong>.
-            Sou apaixonado por tecnologia, com foco em desenvolvimento frontend e interfaces intuitivas.
-          </p>
-          <p>
-            Atualmente, estou construindo projetos em <strong>React</strong> e aprimorando minhas habilidades em JavaScript, CSS moderno e boas práticas de UI/UX.
-          </p>
-          <p>
-            Acompanhe minha trajetória e se conecte comigo pelo LinkedIn:
-            <a href="https://www.linkedin.com/in/otávio-monteiro-465559252/" target="_blank" rel="noreferrer">
-              linkedin.com/in/otávio-monteiro
-            </a>
-          </p>
-        </section>
-
-
-
-
-        {/* HISTÓRICO PROFISSIONAL */}
-        <section id="historico-profissional" className="my-5 mx-3">
-          <h2 className="Sublinhado mb-5">Histórico profissional</h2>
-          <div className="row gx-3">
-            {cargos.map((cargo, index) => (
-              <div key={index} className="mb-5">
-                <h2 className='nomeCargo'><b>{cargo.nomeCargo}</b></h2>
-                <p className='dataCargo'>{cargo.dataCargo}</p>
-                <ul className="listaCargo">
-                  {cargo.responsabilidadesCargos.map((item, idx) => (
-                    <li key={idx} className="responsabilidadesCargo">{item}</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
+        <section className="d-flex justify-content-center align-items-center py-4">
+          <div className="row w-100">
+            <div className="col-md-12 d-flex justify-content-center">
+              <img src={minhaFoto} alt="" className="fotoPerfil" />
+            </div>
+            <div className="col-md-12 mt-4 d-flex justify-content-center">
+              <h1 className="nomeUsuario"><b>Otávio Monteiro</b></h1>
+            </div>
           </div>
         </section>
 
+        <section className='sobreMim py-4  m-3'>
+          <div className="row justify-content-center">
+            <div className="col-md-12 mb-3 text-center">
+              <h2><b>Sobre mim</b></h2>
+            </div>
+            <div className="col-md-10">
+              <div className="cardSobre p-4">
+                <p className="mb-0 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem voluptates recusandae quae corrupti dolor beatae minima dolores, vitae quos voluptate, nam modi laborum. Quod tenetur nam, voluptate reprehenderit alias numquam.</p>
+              </div>
+            </div>
+          </div>
+        </section>
 
+        <section className='historicoProfissional py-4 m-3'>
+          <div className="row justify-content-center">
+            <div className="col-12 text-center mb-4">
+              <h2><b>Histórico Profissional</b></h2>
+            </div>
 
-
-
-        <section id="projetos" className="my-5 mx-3">
-          <h2 className="Sublinhado text-center mb-4">Projetos</h2>
-          <div className="row gy-4 gx-3">
-            {projetos.map((projeto, index) => (
-              <div key={index} className="col-lg-4 col-md-6 col-12">
-                <div className="card h-100">
-                  <img src={projeto.imagem} alt="Foto-Projeto" className="card-img-top img-fluid" />
-                  <div className="card-body d-flex flex-column">
-                    <h5 className="card-title">{projeto.titulo}</h5>
-                    <p className="card-text">{projeto.descricao}</p>
-                    <a href="#" target="_blank" rel="noreferrer" className="btn btn-primary mt-auto">Ver projeto</a>
+            <div className="col-lg-5 col-md-10 m-3">
+              <div className="cardProfissao p-4">
+                <div className="row">
+                  <div className="col-12 text-center">
+                    <h3><b>Desenvolvedor Júnior</b></h3>
+                  </div>
+                  <div className="col-12 text-center mb-3">
+                    <small>20/02/2025 - atualmente</small>
+                  </div>
+                  <div className="col-12 mb-3 text-center text-center">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati, ea. Ex, ducimus hic, eligendi fugiat debitis voluptate unde repudiandae odit corporis magni atque! Dolore aliquam accusantium eveniet magnam nostrum corporis?</p>
+                  </div>
+                  <div className="col-12">
+                    <ul className='competenciasProfissao ps-0 text-center'>
+                      <li className='py-1'><b>Trabalho com VR</b></li>
+                      <li className='py-1'><b>Edição de vídeos</b></li>
+                      <li className='py-1'><b>Desenvolvimento com Unity</b></li>
+                    </ul>
                   </div>
                 </div>
               </div>
-            ))}
+            </div>
+
+            <div className="col-lg-5 col-md-10 m-3">
+              <div className="cardProfissao p-4">
+                <div className="row">
+                  <div className="col-12 text-center">
+                    <h3><b>Desenvolvedor Júnior</b></h3>
+                  </div>
+                  <div className="col-12 text-center mb-3">
+                    <small>20/02/2025 - atualmente</small>
+                  </div>
+                  <div className="col-12 mb-3 text-center">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati, ea. Ex, ducimus hic, eligendi fugiat debitis voluptate unde repudiandae odit corporis magni atque! Dolore aliquam accusantium eveniet magnam nostrum corporis?</p>
+                  </div>
+                  <div className="col-12">
+                    <ul className='competenciasProfissao ps-0 text-center'>
+                      <li className='py-1'><b>Trabalho com VR</b></li>
+                      <li className='py-1'><b>Edição de vídeos</b></li>
+                      <li className='py-1'><b>Desenvolvimento com Unity</b></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
 
-
-
-        {/* CERTIFICADOS E CURSOS */}
-        <section id='certificados' className='my-5 mx-3'>
-          <h2 className='Sublinhado text-center mb-4'>Certificados</h2>
-          <div className="row gy-4 gx-3">
-            <div className="col-md-6">
-              <div className="card-certificados mx-4 mb-3 p-4">
-                <p className='certificado-tittle'>Python Paideia</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolores quam deserunt dicta sequi quisquam nesciunt cumque consequatur nihil reiciendis illum deleniti libero, perferendis ad corrupti soluta mollitia distinctio vel!</p>
-                <a href={certificadoPython} target='_blank' className='btn btn-primary px-5 certificado-button'>Ver certificado</a>
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="card-certificados mx-4 mb-3 p-4">
-                <p className='certificado-tittle'>Python Paideia</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolores quam deserunt dicta sequi quisquam nesciunt cumque consequatur nihil reiciendis illum deleniti libero, perferendis ad corrupti soluta mollitia distinctio vel!</p>
-                <a href={certificadoPython} target='_blank' className='btn btn-primary px-5 certificado-button'>Ver certificado</a>
-              </div>
+        <section className='historicoAcademico py-4  m-3'>
+          <div className="row justify-content-center">
+            <div className="col-12 text-center mb-4">
+              <h2><b>Histórico Acadêmico</b></h2>
             </div>
 
+            <div className="col-lg-5 col-md-10 m-3">
+              <div className="cardProfissao p-4">
+                <div className="row">
+                  <div className="col-12 text-center mb-3">
+                    <img src={imagemFaculdade} alt="" className='fotoHistorico' />
+                  </div>
+                  <div className="col-12 text-center">
+                    <h3><b>Desenvolvimento de Software Multiplataforma</b></h3>
+                  </div>
+                  <div className="col-12 text-center mb-3">
+                    <small>13/02/2025 - 12/12/2027</small>
+                  </div>
+                  <div className="col-12 text-center">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati, ea. Ex, ducimus hic, eligendi fugiat debitis voluptate unde repudiandae odit corporis magni atque! Dolore aliquam accusantium eveniet magnam nostrum corporis?</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-5 col-md-10 m-3">
+              <div className="cardProfissao p-4">
+                <div className="row">
+                  <div className="col-12 text-center mb-3">
+                    <img src={imagemFaculdade} alt="" className='fotoHistorico' />
+                  </div>
+                  <div className="col-12 text-center">
+                    <h3><b>Desenvolvimento de Software Multiplataforma</b></h3>
+                  </div>
+                  <div className="col-12 text-center mb-3">
+                    <small>13/02/2025 - 12/12/2027</small>
+                  </div>
+                  <div className="col-12 text-center">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati, ea. Ex, ducimus hic, eligendi fugiat debitis voluptate unde repudiandae odit corporis magni atque! Dolore aliquam accusantium eveniet magnam nostrum corporis?</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
 
+        <section className='certificados py-4  m-3'>
+          <div className="row justify-content-center">
+            <div className="col-12 text-center mb-4">
+              <h2><b>Certificados</b></h2>
+            </div>
 
+            <div className="col-lg-5 col-md-10 m-3">
+              <div className="cardCertificado p-3 text-center">
+                <img src={fotoCertificado} alt="" className="img-fluid" />
+                <div className="tituloCertificado mt-3">
+                  <h4><b>Desenvolvimento Python</b></h4>
+                </div>
+              </div>
+            </div>
 
-
-
-        {/* CONTATO */}
-        <section id="contato" className="my-5 mx-3">
-          <h2 className="Sublinhado mb-4">Contato</h2>
-          <p>Email: otavio@example.com</p>
-          <p>LinkedIn: <a href="https://linkedin.com/in/otaviomonteiro" target="_blank" rel="noreferrer">linkedin.com/in/otaviomonteiro</a></p>
+            <div className="col-lg-5 col-md-10 m-3">
+              <div className="cardCertificado p-3 text-center">
+                <img src={fotoCertificado} alt="" className="img-fluid" />
+                <div className="tituloCertificado mt-3">
+                  <h4><b>Desenvolvimento Python</b></h4>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
-      </div>
+
+        <section className='projetos py-4  m-3'>
+          <div className="row justify-content-center">
+            <div className="col-12 text-center mb-4">
+              <h2><b>Projetos</b></h2>
+            </div>
+
+            <div className="col-lg-5 col-md-10 m-3">
+              <div className="cardProjetos p-3 text-center">
+                <div className="fotoProjeto">
+                  <img src={fotoProjeto} alt="" className="img-fluid" />
+                </div>
+                <div className="tituloProjeto mt-3">
+                  <h4><b>Chronos Lux</b></h4>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-5 col-md-10 m-3">
+              <div className="cardProjetos p-3 text-center">
+                <div className="fotoProjeto">
+                  <img src={fotoProjeto} alt="" className="img-fluid" />
+                </div>
+                <div className="tituloProjeto mt-3">
+                  <h4><b>Chronos Lux</b></h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
